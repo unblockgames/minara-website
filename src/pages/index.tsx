710e4831fab1c80b3217301321ae028d00e2e0f5
unblockgames <unblockgames@gmail.com>
2024-06-30 21:20:57 -0500

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import ServiceCard from "@/components/ServiceCard";
 import Tactic from "@/components/Tactic";
 import TeacherCard from "@/components/TeacherCard";
 import Image from "next/image";
+import { useRef } from "react";
+import { PopupButton } from "react-calendly";
 
 const teachers = [
   {
@@ -138,10 +140,20 @@ const tactics = [
 ];
 
 export default function Home() {
+  const rootRef = useRef(null);
   return (
     <main
-      className={`flex flex-col h-full w-screen justify-center overflow-hidden`}
+      id="root"
+      ref={rootRef}
+      className={`flex flex-col h-full w-screen justify-center overflow-hidden relative`}
     >
+      <PopupButton
+        className="bg-orange-500 w-32 p-2 rounded-lg font-bold text-stone-100 shadow-sm shadow-slate-700 fixed bottom-0 right-0 m-8 z-[100]"
+        url="https://calendly.com/minara-beck/30min"
+        //@ts-ignore
+        rootElement={rootRef.current}
+        text="Let's Talk!"
+      />
       <section>
         <div className="flex flex-col items-center m-4">
           <h1 className={`text-2xl md:text-4xl`}>English German Russian</h1>
